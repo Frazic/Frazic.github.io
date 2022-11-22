6a8ca9242cbb89719b3a951e6d679f9099076d04
Frazic <bruylant.joshua@gmail.com>
2022-11-22 13:20:01 +0100

    Got mousePressed and mouseWheel working

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -4,24 +4,32 @@ import p5 from "p5";
 
 export default component$(() => {
   const myp5 = $(() => new p5((sketch) => {
-
-    const x = 100;
-    const y = 100;
+    let red = 0;
 
     sketch.setup = () => {
-      const canvas = sketch.createCanvas(200, 200);
+      const canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
       canvas.parent("p5sketch");
+      canvas.position(0, 0)
+      canvas.style("z-index", "-1")
     };
+
+    sketch.windowResized = () => {
+      sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight)
+    }
 
     sketch.draw = () => {
       sketch.background(0);
-      sketch.fill(255)
+      sketch.fill(red, 0, 0)
       if (sketch.mouseIsPressed) {
         sketch.fill(100, 100, 10);
       }
       sketch.rect(sketch.mouseX, sketch.mouseY, 50, 50);
       // sketch.noLoop();
     };
+
+    sketch.mouseWheel = (event) => {
+      red += event.delta;
+    }
   }));
   useClientEffect$(() => {
     myp5();
