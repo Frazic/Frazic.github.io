020f496e695b941fc4a20913c6817a7fd89a3c0f
Frazic <bruylant.joshua@gmail.com>
2022-11-22 14:16:32 +0100

    Got Perlin noise graph showing

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -4,31 +4,44 @@ import p5 from "p5";
 
 export default component$(() => {
   const myp5 = $(() => new p5((sketch) => {
-    let red = 0;
 
     sketch.setup = () => {
       const canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
       canvas.parent("p5sketch");
       canvas.position(0, 0)
       canvas.style("z-index", "-1")
+      sketch.noiseDetail(1);
     };
 
     sketch.windowResized = () => {
       sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight)
     }
 
+    const inc = 0.01;
+    let scroll = 0;
+
     sketch.draw = () => {
+      const start = sketch.frameCount / 200;
       sketch.background(0);
-      sketch.fill(red, 0, 0)
-      if (sketch.mouseIsPressed) {
-        sketch.fill(100, 100, 10);
+      sketch.fill(100, 100, 10);
+      sketch.noStroke();
+      // sketch.noFill();
+      sketch.beginShape()
+      let xoff = 0;
+      for (let x = 0; x <= sketch.width; x++) {
+        let y = sketch.map(sketch.noise(xoff, start), 0, 1, sketch.height, sketch.height / 100)
+        if (x === 0 || x === sketch.width) {
+          y = sketch.height
+        }
+        sketch.vertex(x, y);
+
+        xoff += inc;
       }
-      sketch.rect(sketch.mouseX, sketch.mouseY, 50, 50);
-      // sketch.noLoop();
+      sketch.endShape();
     };
 
-    sketch.mouseWheel = (event) => {
-      red += event.delta;
+    sketch.mouseWheel = (event: any) => {
+      scroll += event.delta;
     }
   }));
   useClientEffect$(() => {
