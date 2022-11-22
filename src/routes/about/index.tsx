0a99e7190b962ece91d6dcc400d76b82d3291995
Frazic <bruylant.joshua@gmail.com>
2022-11-22 14:49:57 +0100

    Got coloured waves that scroll up

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -23,19 +23,20 @@ export default component$(() => {
     sketch.draw = () => {
       const start = sketch.frameCount / 200;
       sketch.background(0);
-      sketch.stroke(255);
-      sketch.fill(100, 100, 10);
-      // sketch.noStroke();
+      sketch.strokeWeight(2);
       for (let shapeNumber = 1; shapeNumber < 4; shapeNumber++) {
+        if (shapeNumber === 1) sketch.fill("#260443")
+        if (shapeNumber === 2) sketch.fill("#CB0343")
+        if (shapeNumber === 3) sketch.fill("#FF6A2A")
         sketch.beginShape()
 
         // This determines their starting position and allows waves to scroll up
-        const yOffset = sketch.min(((sketch.height / 2) - scroll) - (shapeNumber * sketch.height), sketch.height / 1.35)
+        const yOffset = sketch.min(((sketch.height / 2) - scroll) - (shapeNumber * (sketch.height) - sketch.height * 0.5), sketch.height / (1.3 * shapeNumber))
 
         // Draw shape, y being a factor of x and noise
         let xoff = 0;
         for (let x = 0; x <= sketch.width; x++) {
-          let y = sketch.map(sketch.noise(xoff, start), 0, 1, sketch.height, sketch.height / 2) - yOffset
+          let y = sketch.map(sketch.noise(xoff + 10 * shapeNumber, start), 0, 1, sketch.height, sketch.height / 2) - yOffset
 
           // This clamps the start and end position to have a uniform fill
           if (x === 0 || x === sketch.width) {
