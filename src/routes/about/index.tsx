e9b037f3e8012cf826cea48eb14ae18ba383e2f7
Frazic <bruylant.joshua@gmail.com>
2022-11-22 14:40:00 +0100

    Have scrolling waves

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -17,31 +17,43 @@ export default component$(() => {
       sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight)
     }
 
-    const inc = 0.01;
+    const inc = 0.007;
     let scroll = 0;
 
     sketch.draw = () => {
       const start = sketch.frameCount / 200;
       sketch.background(0);
+      sketch.stroke(255);
       sketch.fill(100, 100, 10);
-      sketch.noStroke();
-      // sketch.noFill();
-      sketch.beginShape()
-      let xoff = 0;
-      for (let x = 0; x <= sketch.width; x++) {
-        let y = sketch.map(sketch.noise(xoff, start), 0, 1, sketch.height, sketch.height / 100)
-        if (x === 0 || x === sketch.width) {
-          y = sketch.height
-        }
-        sketch.vertex(x, y);
+      // sketch.noStroke();
+      for (let shapeNumber = 1; shapeNumber < 4; shapeNumber++) {
+        sketch.beginShape()
 
-        xoff += inc;
+        // This determines their starting position and allows waves to scroll up
+        const yOffset = sketch.min(((sketch.height / 2) - scroll) - (shapeNumber * sketch.height), sketch.height / 1.35)
+
+        // Draw shape, y being a factor of x and noise
+        let xoff = 0;
+        for (let x = 0; x <= sketch.width; x++) {
+          let y = sketch.map(sketch.noise(xoff, start), 0, 1, sketch.height, sketch.height / 2) - yOffset
+
+          // This clamps the start and end position to have a uniform fill
+          if (x === 0 || x === sketch.width) {
+            y = sketch.height
+          }
+          sketch.vertex(x, y);
+
+          xoff += inc;
+        }
+        sketch.endShape();
       }
-      sketch.endShape();
     };
 
     sketch.mouseWheel = (event: any) => {
-      scroll += event.delta;
+      scroll -= event.delta * 1;
+
+      // Clamp scroll so we can't go below initial position
+      if (scroll >= 0) scroll = 0;
     }
   }));
   useClientEffect$(() => {
