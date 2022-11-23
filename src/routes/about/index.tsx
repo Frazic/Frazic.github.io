1ac17bc93effab716a1524a7c78b8b219b04cb40
Frazic <bruylant.joshua@gmail.com>
2022-11-23 11:45:53 +0100

    FUNCTIONAL SAVED BY CSS ONCE AGAIN

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,69 +1,36 @@
-import { $, component$, useClientEffect$ } from "@builder.io/qwik";
+import { component$, useClientEffect$, useStyles$, $ } from "@builder.io/qwik";
 import { DocumentHead } from "@builder.io/qwik-city";
-import p5 from "p5";
+import { PerlinWave } from "./perlinWave";
+import style from "./about.css?inline"
 
 export default component$(() => {
-  const myp5 = $(() => new p5((sketch) => {
+  // if (shapeNumber === 1) sketch.fill("#260443")
+  // if (shapeNumber === 2) sketch.fill("#CB0343")
+  // if (shapeNumber === 3) sketch.fill("#FF6A2A")
+  useStyles$(style);
 
-    sketch.setup = () => {
-      const canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-      canvas.parent("p5sketch");
-      canvas.position(0, 0)
-      canvas.style("z-index", "-1")
-      sketch.noiseDetail(1);
-    };
+  const FixTopSections$ = $((wrapperDiv: HTMLDivElement) => {
+    const sections = [...wrapperDiv.querySelectorAll("section")];
+    const scrollY = window.scrollY;
 
-    sketch.windowResized = () => {
-      sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight)
-    }
+    // All sections above the scrollY limit, the last one is the current section
+    const aboveSections = sections.filter((section) => section.offsetTop <= scrollY);
+    aboveSections.map((section) => { section.classList.add("fixed") });
 
-    const inc = 0.007;
-    let scroll = 0;
+    const belowSections = sections.filter((section) => section.offsetTop > scrollY);
+    belowSections.map((section) => { section.classList.remove("fixed") });
+  })
 
-    sketch.draw = () => {
-      const start = sketch.frameCount / 200;
-      sketch.background(0);
-      sketch.strokeWeight(2);
-      for (let shapeNumber = 1; shapeNumber < 4; shapeNumber++) {
-        if (shapeNumber === 1) sketch.fill("#260443")
-        if (shapeNumber === 2) sketch.fill("#CB0343")
-        if (shapeNumber === 3) sketch.fill("#FF6A2A")
-        sketch.beginShape()
-
-        // This determines their starting position and allows waves to scroll up
-        const yOffset = sketch.min(((sketch.height / 2) - scroll) - (shapeNumber * (sketch.height) - sketch.height * 0.5), sketch.height / (1.3 * shapeNumber))
-
-        // Draw shape, y being a factor of x and noise
-        let xoff = 0;
-        for (let x = 0; x <= sketch.width; x++) {
-          let y = sketch.map(sketch.noise(xoff + 10 * shapeNumber, start), 0, 1, sketch.height, sketch.height / 2) - yOffset
-
-          // This clamps the start and end position to have a uniform fill
-          if (x === 0 || x === sketch.width) {
-            y = sketch.height
-          }
-          sketch.vertex(x, y);
-
-          xoff += inc;
-        }
-        sketch.endShape();
-      }
-    };
-
-    sketch.mouseWheel = (event: any) => {
-      scroll -= event.delta * 1;
-
-      // Clamp scroll so we can't go below initial position
-      if (scroll >= 0) scroll = 0;
-    }
-  }));
   useClientEffect$(() => {
-    myp5();
+    PerlinWave({ id: "perlin1", colour: "#260443", offsetTop: 1 / 3 })
+    PerlinWave({ id: "perlin2", colour: "#CB0343", offsetTop: 1 / 2 })
   })
   return (
     <>
       <h1>About</h1>
-      <div id="p5sketch">
+      <div className="wrapper" document:onScroll$={(_, el: HTMLDivElement) => FixTopSections$(el)}>
+        <section id="perlin1" className="about" style={{ "width": "100vw", "height": "100vh" }} />
+        <section id="perlin2" className="about" style={{ "width": "100vw", "height": "100vh" }} />
       </div>
     </>
   );
