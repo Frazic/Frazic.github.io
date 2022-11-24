1e4bb011b587a168dc2ba4314b80abc19b7682d2
Frazic <bruylant.joshua@gmail.com>
2022-11-24 13:45:34 +0100

    Added some nice colours

--- src/routes/skills/index.tsx
+++ src/routes/skills/index.tsx
@@ -68,26 +68,26 @@ footer {\
               isActive={isTree1Active.value}
               tooltipStore={tooltipStore}
               items={[
-                { name: "JavaScript", tooltip: "", iconSrc: "icons/javascript.png" },
+                { name: "JavaScript", tooltip: "", iconSrc: "icons/javascript.png", background: "#20182E" },
                 { name: "link" },
-                { name: "React", tooltip: "", iconSrc: "icons/react.png" },
+                { name: "React", tooltip: "", iconSrc: "icons/react.png", background: "#2B1B46" },
                 { name: "link" },
-                { name: "Next", tooltip: "", iconSrc: "icons/next-js.png" },
+                { name: "Next", tooltip: "", iconSrc: "icons/next-js.png", background: "#28114F" },
                 { name: "link" },
-                { name: "Qwik", tooltip: "", iconSrc: "icons/qwik.svg" },
+                { name: "Qwik", tooltip: "", iconSrc: "icons/qwik.svg", background: "#320F6C" },
               ]}
             />
             <TalentColumn
               isActive={isTree1Active.value}
               tooltipStore={tooltipStore}
               items={[
-                { name: "ASP.NET", tooltip: "", iconSrc: "icons/asp-net.png" },
+                { name: "ASP.NET", tooltip: "", iconSrc: "icons/asp-net.png", background: "#32182B" },
                 { name: "space" },
-                { name: "PHP", tooltip: "", iconSrc: "icons/php.png" },
+                { name: "PHP", tooltip: "", iconSrc: "icons/php.png", background: "#4C193E" },
                 { name: "space" },
-                { name: "HTML", tooltip: "", iconSrc: "icons/html.png" },
+                { name: "HTML", tooltip: "", iconSrc: "icons/html.png", background: "#570B42" },
                 { name: "link" },
-                { name: "CSS", tooltip: "", iconSrc: "icons/css.png" },
+                { name: "CSS", tooltip: "", iconSrc: "icons/css.png", background: "#770658" },
               ]}
             />
           </div>
@@ -108,24 +108,24 @@ footer {\
               isActive={isTree2Active.value}
               tooltipStore={tooltipStore}
               items={[
-                { name: "Python", tooltip: "", iconSrc: "icons/python.png" },
+                { name: "Python", tooltip: "", iconSrc: "icons/python.png", background: "#442B20" },
                 { name: "space" },
-                { name: "SQL", tooltip: "", iconSrc: "icons/sql.png" },
+                { name: "SQL", tooltip: "", iconSrc: "icons/sql.png", background: "#663721" },
                 { name: "space" },
-                { name: "Bash", tooltip: "", iconSrc: "icons/bash.png" },
+                { name: "Bash", tooltip: "", iconSrc: "icons/bash.png", background: "#742E0F" },
                 { name: "space" },
-                { name: "Java", tooltip: "", iconSrc: "icons/java.png" },
+                { name: "Java", tooltip: "", iconSrc: "icons/java.png", background: "#9F3708" },
               ]}
             />
             <TalentColumn
               isActive={isTree2Active.value}
               tooltipStore={tooltipStore}
               items={[
-                { name: "C", tooltip: "", iconSrc: "icons/c.png" },
+                { name: "C", tooltip: "", iconSrc: "icons/c.png", background: "#443A20" },
                 { name: "link" },
-                { name: "C++", tooltip: "", iconSrc: "icons/c++.png" },
+                { name: "C++", tooltip: "", iconSrc: "icons/c++.png", background: "#665421" },
                 { name: "link" },
-                { name: "C#", tooltip: "", iconSrc: "icons/csharp.png" },
+                { name: "C#", tooltip: "", iconSrc: "icons/csharp.png", background: "#745A0F" },
               ]}
             />
           </div>
@@ -146,26 +146,26 @@ footer {\
               isActive={isTree3Active.value}
               tooltipStore={tooltipStore}
               items={[
-                { name: "CAD", tooltip: "", iconSrc: "icons/solidworks.png" },
+                { name: "CAD", tooltip: "", iconSrc: "icons/solidworks.png", background: "#3B411F" },
                 { name: "space" },
-                { name: "Linux", tooltip: "", iconSrc: "icons/linux.png" },
+                { name: "Linux", tooltip: "", iconSrc: "icons/linux.png", background: "#576320" },
                 { name: "link" },
-                { name: "Git", tooltip: "", iconSrc: "icons/github.png" },
+                { name: "Git", tooltip: "", iconSrc: "icons/github.png", background: "#5F700F" },
                 { name: "link" },
-                { name: "RPi", tooltip: "", iconSrc: "icons/raspberry-pi.png" },
+                { name: "RPi", tooltip: "", iconSrc: "icons/raspberry-pi.png", background: "#809907" },
               ]}
             />
             <TalentColumn
               isActive={isTree3Active.value}
               tooltipStore={tooltipStore}
               items={[
-                { name: "Electronics", tooltip: "", iconSrc: "icons/electronics.png", background: "" },
+                { name: "Electronics", tooltip: "", iconSrc: "icons/electronics.png", background: "#15232B" },
                 { name: "link" },
-                { name: "Arduino", tooltip: "", iconSrc: "icons/arduino.png" },
+                { name: "Arduino", tooltip: "", iconSrc: "icons/arduino.png", background: "#173241" },
                 { name: "link" },
-                { name: "Radio", tooltip: "", iconSrc: "icons/radio.png" },
+                { name: "Radio", tooltip: "", iconSrc: "icons/radio.png", background: "#0D354A" },
                 { name: "space" },
-                { name: "Network", tooltip: "", iconSrc: "icons/network.png" },
+                { name: "Network", tooltip: "", iconSrc: "icons/network.png", background: "#0A4565" },
               ]}
             />
           </div>
