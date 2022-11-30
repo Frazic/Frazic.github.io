6c9c9e3baf2414143d1e52e9e1b9a068ef84abff
Frazic <bruylant.joshua@gmail.com>
2022-11-30 11:24:38 +0100

    Trying the meta tags

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -11,11 +11,9 @@ export const RouterHead = component$(() => {
   return (
     <>
       <title>{head.title}</title>
-      <link rel="canonical" href={loc.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="Cache-control" content="no-cache"></meta>
-      <link rel="icon" type="image/png" href="/favicon.ico" />
 
+      <link rel="canonical" href={loc.href} />
+      <link rel="icon" type="image/png" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
@@ -23,10 +21,47 @@ export const RouterHead = component$(() => {
         rel="stylesheet"
       />
 
-      <meta property="og:site_name" content="Joshua Bruylant's Portfolio" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="Cache-control" content="no-cache"></meta>
+      <meta charSet="UTF-8" />
+
+      {/* <!-- Primary Meta Tags --> */}
+      <meta name="title" content="Frazic - Portfolio" />
+      <meta name="description" content="Skills, projects and a bit about himself" />
+      <meta name="author" content="Frazic" />
+      <meta name="robots" content="index, follow" />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="frazic.github.io" />
+      <meta property="og:title" content="Frazic - Portfolio" />
+      <meta property="og:description" content="Skills, projects and a bit about himself" />
+      <meta property="og:image" content="/josh-photo.webp" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="frazic.github.io" />
+      <meta property="twitter:title" content="Frazic - Portfolio" />
+      <meta property="twitter:description" content="Skills, projects and a bit about himself" />
+      <meta property="twitter:image" content="/josh-photo.webp" />
+
+      {/* FACEBOOK */}
+      {/* <meta property="og:title" content="Frazic - Portfolio" />
+      <meta property="og:image" content="/josh-photo.webp" />
+      <meta property="og:description" content="Skills, projects and a bit about himself" /> */}
+
+
+      {/* TWITTER */}
+      {/* <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@frazicdev" />
+      <meta name="twitter:creator" content="@frazicdev" />
+      <meta name="twitter:title" content="Frazic - Portfolio" />
+      <meta name="twitter:description" content="Skills, projects and a bit about himself" />
+      <meta name="twitter:image" content="/josh-photo.webp" /> */}
+
+      {/* <meta property="og:site_name" content="Frazic's Portfolio" /> */}
       {/* <meta name="twitter:site" content="@Frazic" /> */}
       {/* <meta name="twitter:title" content="Frazic" /> */}
-      <meta property="icons" content="Icons by Icons8 (https://icons8.com)" />
 
       {head.meta.map((m) => (
         <meta {...m} />
