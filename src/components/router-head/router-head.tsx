d927dbeae906be7ebd22cfc19f8a8a3f4bcb78a1
Frazic <bruylant.joshua@gmail.com>
2022-11-30 11:33:56 +0100

    Changed url in meta tags

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -28,19 +28,19 @@ export const RouterHead = component$(() => {
       {/* <!-- Primary Meta Tags --> */}
       <meta name="title" content="Frazic - Portfolio" />
       <meta name="description" content="Skills, projects and a bit about himself" />
-      <meta name="author" content="Frazic" />
+      <meta name="author" content="Joshua Bruylant" />
       <meta name="robots" content="index, follow" />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="frazic.github.io" />
+      <meta property="og:url" content="https://frazic.github.io" />
       <meta property="og:title" content="Frazic - Portfolio" />
       <meta property="og:description" content="Skills, projects and a bit about himself" />
       <meta property="og:image" content="https://frazic.github.io/josh-photo.webp" />
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="frazic.github.io" />
+      <meta property="twitter:url" content="https://frazic.github.io" />
       <meta property="twitter:title" content="Frazic - Portfolio" />
       <meta property="twitter:description" content="Skills, projects and a bit about himself" />
       <meta property="twitter:image" content="https://frazic.github.io/josh-photo.webp" />
